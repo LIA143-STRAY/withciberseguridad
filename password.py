09bf12caf96cf4dd4d25bb88023ae8aeb545f020
LIA143-STRAY <liainthegalaxy@gmail.com>
2026-04-19 14:30:53 +0200

    mejora: validacion de logitud minima

--- password.py
+++ password.py
@@ -1,7 +1,19 @@
 import random
 import string
-longitud = int(input("cuantos caracteres quieres: "))
-caracteres = string.ascii_letters + string.digits + string.punctuation
-password = " "
-for i in range(longitud): password += random.choice(caracteres)
+while True:
+     try:
+         longitud = int(input("Cuantos caracteres quieres: "))
+         if longitud < 6:
+              print("Muy corta, intenta de nuevo")
+         else:
+              break
+     except:
+         print("Por favor ingresa un numero valido")
+usar_simbolos = input("Quieres simbolos? (s/n): ")
+caracteres = string.ascii_letters + string.digits
+if usar_simbolos.lower() == "s":
+      caracteres += string.punctuation
+password = ""
+for i in range(longitud):
+      password += random.choice(caracteres)
 print("Tu contraseña es:", password)
